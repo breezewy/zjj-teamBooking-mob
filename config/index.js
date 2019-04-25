440e0a0c035beee4f55180c05666594ba43f01d9
刘友友 <1624198787@qq.com>
2019-04-25 16:14:03 +0800

    修改config文件为localhost

--- config/index.js
+++ config/index.js
@@ -13,7 +13,7 @@ module.exports = {
     proxyTable: {},
 
     // Various Dev Server settings
-    host: '193.1.0.98', // can be overwritten by process.env.HOST
+    host: 'localhost', // can be overwritten by process.env.HOST
     port: 8889, // can be overwritten by process.env.PORT, if port is in use, a free one will be determined
     autoOpenBrowser: false,
     errorOverlay: true,
