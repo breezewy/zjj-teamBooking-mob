88e93404111e06fc08218a187d9d24ffb8c7c17e
liweiyang <15658150268@163.com>
2019-05-28 16:12:11 +0800

    修复gl分支冲突

--- config/dev.env.js
+++ config/dev.env.js
@@ -5,15 +5,12 @@ const prodEnv = require('./prod.env')
 module.exports = merge(prodEnv, {
   NODE_ENV: '"development"',
   // API_ROOT: '"http://192.168.11.85:20001"'
-<<<<<<< HEAD
   // API_ROOT: '"http://193.1.1.227:20003"'
   // API_ROOT: '"https://scteam-m.dmqwl.com"'
   // API_ROOT: '"https://glteam-api.dmqwl.com"'
   API_ROOT:'"http://192.168.11.85:20004"'
-=======
   // API_ROOT: '"http://193.1.1.227:20001"'
   // API_ROOT: '"http://193.1.1.227:20002"'
-  API_ROOT: '"https://glteam-api.dmqwl.com"'
->>>>>>> 54b1af8201725d322ca31d173b2c3ca45c0b3cb5
+  // API_ROOT: '"https://glteam-api.dmqwl.com"'
 })
 
