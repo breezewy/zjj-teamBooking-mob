8cc0c2c099c97c00d2d73682e3be5edcd215f47c
liweiyang <15658150268@163.com>
2019-06-25 17:07:28 +0800

    API修改

--- config/prod.env.js
+++ config/prod.env.js
@@ -1,5 +1,6 @@
 'use strict'
 module.exports = {
   NODE_ENV: '"production"',
-  API_ROOT: '"https://glteam-api.dmqwl.com"'
+  // API_ROOT: '"https://glteam-api.dmqwl.com"'
+  API_ROOT: '"https://zjjteam-api.dmqwl.com"'
 }
